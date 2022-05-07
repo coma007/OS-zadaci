660e1235554afb692212371ac2000bb247c74bf7
Milica <milicasladakovic7@gmail.com>
2022-05-07 06:25:39 +0200

    Rjesenje 1. zadatka

--- v01-Niti/01_pozdrav_iz_niti.cpp
+++ v01-Niti/01_pozdrav_iz_niti.cpp
@@ -1,3 +1,16 @@
 /* Napraviti program koji kreira jednu nit i u okviru nje ispisuje proizvoljnu
  * recenicu.
  */
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void stampa() {
+	cout << "Hello, world !" << endl;
+}
+
+int main() ;{
+	thread t(stampa);
+	t.join();
+	return 0;
+}
