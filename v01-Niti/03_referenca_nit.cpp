bb11a1acbf3d9baf6967bcffec22c33940d48871
Milica <milicasladakovic7@gmail.com>
2022-05-07 06:45:35 +0200

    Rjesenje 3. zadatka

--- v01-Niti/03_referenca_nit.cpp
+++ v01-Niti/03_referenca_nit.cpp
@@ -4,3 +4,20 @@
  *
  * Ispisati vrednost broja nakon zavrsetka rada niti
  */
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void inkrement(int &broj) {
+	broj++;
+}
+
+int main() {
+	int a = 5;
+	thread t(inkrement, ref(a));
+	t.join();
+	cout << a;
+	return 0;
+}
