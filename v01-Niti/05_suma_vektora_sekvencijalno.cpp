37040011e9210bce9f6510ba42ef76882951ae00
Milica <milicasladakovic7@gmail.com>
2022-05-08 16:00:08 +0200

    Rjesenje 5. zadatka

--- v01-Niti/05_suma_vektora_sekvencijalno.cpp
+++ v01-Niti/05_suma_vektora_sekvencijalno.cpp
@@ -9,12 +9,21 @@
  */
 
 #include <iostream>
-#include <thread>
+#include <vector>
 
 using namespace std;
 
 typedef vector<double>::const_iterator ci;
 
 void f(ci begin, ci end, double& zbir) {
-	
+ 	for (ci i = begin; i != end; i++) {
+		zbir += *i;
+	}
+}
+
+int main() {
+	vector<double> v = {1, 2, 3, 4, 5, 6, 7};
+	double zbir;
+	f(v.begin(), v.end(), zbir);
+	cout << zbir << endl;
 }
