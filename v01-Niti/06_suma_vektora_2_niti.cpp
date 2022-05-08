f3ff45d0411c84bb5a57abec7b89a39f85360621
Milica <milicasladakovic7@gmail.com>
2022-05-08 16:24:20 +0200

    Rjesenje 6. zadatka

--- v01-Niti/06_suma_vektora_2_niti.cpp
+++ v01-Niti/06_suma_vektora_2_niti.cpp
@@ -13,3 +13,27 @@
  * Napomena: ovakva optimizacija sumiranja je znacajna kada se radi na
  * dvojezgarnom procesoru za vektore velike duzine.
  */
+
+#include <iostream>
+#include <vector>
+#include <thread>
+
+using namespace std;
+typedef vector<double>::const_iterator ci;
+
+void f(ci begin, ci end, double& zbir) {
+	for (; begin != end; begin++) {
+		zbir += *begin;	
+	}
+}
+
+int main() {
+	vector<double> v = {1, 2, 3, 4, 5, 6};
+	double zbir1, zbir2;
+	thread t1(f, v.begin(), v.begin() + v.size()/2, ref(zbir1));
+	thread t2(f, v.begin() + v.size()/2, v.end(), ref(zbir2));
+	t1.join();
+	t2.join();
+	cout << zbir1 + zbir2;
+	return 0;
+}
