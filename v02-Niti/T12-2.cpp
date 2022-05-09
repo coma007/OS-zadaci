87b7912bf0b0a4a233be285ff6a4ff540ac48c81
Milica <milicasladakovic7@gmail.com>
2022-05-09 04:42:30 +0200

    Rjesenje 2. zadatka

--- v02-Niti/T12-2.cpp
+++ v02-Niti/T12-2.cpp
@@ -2,3 +2,25 @@
  * zatim stvara zadati broj niti. Pri instanciranju nit dobija redni broj pod
  * kojim je stvorena. Svaka nit ispisuje svoj redni broj i svoj identifikator.
  */
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void nit(int rbr) {
+	cout << "Ja sam nit " << rbr << endl;
+}
+
+int main() {
+	cout << "Unesi broj niti : ";
+	int broj;
+	cin >> broj;
+	thread t[broj];
+	for (int i = 0; i != broj; i++) {
+		t[i] = thread(nit, i);
+	}
+	for (int i = 0; i != broj; i++) {
+		t[i].join();
+	}
+}
