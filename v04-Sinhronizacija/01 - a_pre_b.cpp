f8597fc5e4c6d9c5dd5a258b5eb417380eba3996
Milica <milicasladakovic7@gmail.com>
2022-05-13 01:57:13 +0200

    Rjesenje 1. zadatka

--- v04-Sinhronizacija/01 - a_pre_b.cpp
+++ v04-Sinhronizacija/01 - a_pre_b.cpp
@@ -3,3 +3,33 @@
  * Nit b ispisuje: "Ja sam nit b."
  * Obezbediti da se uvek izvrsi prvo nit a.
  */
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+mutex m;
+condition_variable cv;
+bool a_gotov = false;
+
+void a() {
+	unique_lock<mutex> l(m);
+	cout << "Ja sam nit a." << endl;
+	a_gotov = true;
+	cv.notify_one();
+}
+
+void b() {
+	unique_lock<mutex> l(m);
+	while (!a_gotov) cv.wait(l);
+	cout << "Ja sam nit b." << endl;
+}
+
+int main() {
+	thread ta(a), tb(b);
+	ta.join(); tb.join();
+	return 0;
+}
